0951b4dcdfa42a397a952357fa46abe280e34920
Doggo <69554869+doggo404@users.noreply.github.com>
2022-01-12 17:48:09 +0000

    Create size-calc.py

---
+++ size-calc.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import hurry.filesize
+
+cookie = 'session='
+headers = {'cookie': cookie}
+count = 1
+size = 0
+while count < 35000: #change the numer to the id of the newest upload
+    count = count + 1
+    info_url = 'https://bemaniso.ws/ajax.php?action=torrent&id=' + str(count)
+    torrent_status = requests.get(info_url, headers=headers)
+    torrent_info = json.loads(torrent_status.content)
+    status = torrent_info['status']
+    if status == 'success':
+            size_bytes = torrent_info['response']['torrent']['size']
+            size = size + size_bytes
+            torrent_size = hurry.filesize.size(size)
+            print(f'Total size up to https://bemaniso.ws/torrents.php?torrentid={count}  :  {torrent_size} , {size}B')
